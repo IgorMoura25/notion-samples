b0df97c4562f670a5a9d7ca7a919f76f00d4de4d
Igor Moura <igor.moura254@hotmail.com>
2023-11-06 22:23:29 -0300

    adding adapter pattern

---
+++ architecture-design/design-patterns/GangOfFourPatterns/Structural/Adapter/Program.cs
@@ -0,0 +1,11 @@
+﻿using Adapter;
+
+var adaptee = new JsonLibrary();
+
+ILibrary adapter = new Adapter.XmlAdapter(adaptee);
+
+// Cliente chamando a interface diretamente
+// e o adapter se encarregando de encapsular o Adaptee
+// e converter seu código tornando compatível o compartilhamento
+// entre Cliente vs Interface incompatível
+Console.WriteLine(adapter.ReturnXMLRequest());
---
+++ architecture-design/design-patterns/GangOfFourPatterns/Structural/Adapter/Structure.cs
@@ -0,0 +1,43 @@
+﻿namespace Adapter;
+
+// A classe que será Adaptada contém comportamentos que queremos utilizar
+// mas sua interface é incompatível com o código atual do cliente que só aceita XML
+public class JsonLibrary // (Service)
+{
+    public string ReturnJsonRequest()
+    {
+        return "Here's your request!";
+    }
+}
+
+// A classe que será usada para colaborar com o cliente
+public interface ILibrary // (Client Interface)
+{
+    string ReturnXMLRequest();
+}
+
+// O Adapter faz a classe que será Adaptada (Service) ser compatível com a
+// classe que o cliente usa (Client Interface)
+public class XmlAdapter : ILibrary
+{
+    private readonly JsonLibrary _adaptee;
+
+    public XmlAdapter(JsonLibrary adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public string ReturnXMLRequest()
+    {
+        var json = _adaptee.ReturnJsonRequest();
+
+        var xml = ConvertToXml(json);
+
+        return $"This is '{xml}' that was JSON";
+    }
+
+    private string ConvertToXml(string json)
+    {
+        return "XML";
+    }
+}
