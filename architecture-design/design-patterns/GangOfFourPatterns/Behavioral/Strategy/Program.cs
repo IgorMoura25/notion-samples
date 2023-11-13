e85cfe60d59851baeac88c1654f50da2e016df0e
Igor Moura <igor.moura254@hotmail.com>
2023-11-13 13:17:10 -0300

    adding strategy pattern

---
+++ architecture-design/design-patterns/GangOfFourPatterns/Behavioral/Strategy/Program.cs
@@ -0,0 +1,22 @@
+﻿// O código do cliente escolhe uma estratégia concreta e a passa para o
+// contexto. O cliente deve estar ciente das diferenças entre
+// estratégias para fazer a escolha certa.
+using Strategy;
+
+var context = new Context();
+
+Console.WriteLine("Client: Escolhendo estratégia de Ônibus.");
+context.SetStrategy(new BusRouteStrategy());
+context.PlanRoute(latitude: 100, longitude: 500);
+
+Console.WriteLine();
+
+Console.WriteLine("Client: Escolhendo estratégia de Bike.");
+context.SetStrategy(new BikeRouteStrategy());
+context.PlanRoute(latitude: 100, longitude: 500);
+
+Console.WriteLine();
+
+Console.WriteLine("Client: Escolhendo estratégia de Carro.");
+context.SetStrategy(new CarRouteStrategy());
+context.PlanRoute(latitude: 100, longitude: 500);
---
+++ architecture-design/design-patterns/GangOfFourPatterns/Behavioral/Strategy/Structure.cs
@@ -0,0 +1,74 @@
+﻿namespace Strategy;
+
+// A interface de estratégia declara todas as operações que são comuns para todas as "versões" de um mesmo algoritmo.
+// Neste exemplo a família de algorítmos é a "Rota" e é dentro dessa família
+// que as rotas/estratégias de Ônibus, Bicicleta e Carro farão parte.
+// Tanto ônibus quanto bicicleta servem para levar uma pessoa de um ponto A para um ponto B (família de algoritmo),
+// mas as formas que são executadas ou as "estratégias" são diferentes.
+//
+// O Contexto irá usar essa interface para chamar a classe concreta da estratégia.
+public interface IRouteStrategy
+{
+    string BuildRoute(int latitude, int longitude);
+}
+
+// O Contexto define uma "ponte" entre o cliente e a estratégia
+public class Context
+{
+    // O Contexto mantém uma referência a uma das estratégias concretas.
+    // Ele não sabe a estratégia concreta, e deve trabalhar com todas elas através da inferface.
+    private IRouteStrategy? _strategy;
+
+    public Context()
+    {
+    }
+
+    // Normalmente, o Contexto aceita uma estratégia através do construtor, mas
+    // também fornece um setter para alterá-lo em tempo de execução.
+    public Context(IRouteStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    // Normalmente, o Contexto permite substituir uma Estratégia em tempo de execução.
+    public void SetStrategy(IRouteStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    // O Context delega algum trabalho ao objeto Strategy em vez de
+    // implementar múltiplas versões do algoritmo por conta própria.
+    public void PlanRoute(int latitude, int longitude)
+    {
+        Console.WriteLine("Context: Planejando uma rota utilizando uma estratégia que não sabe qual é");
+        var result = _strategy.BuildRoute(latitude, longitude);
+
+        Console.WriteLine(result);
+    }
+}
+
+// Estratégias Concretas implementam o algoritmo seguindo a Interface de estratégia.
+// Fazendo parte da família de algoritmos.
+public class BusRouteStrategy : IRouteStrategy
+{
+    public string BuildRoute(int latitude, int longitude)
+    {
+        return "Ônibus: 20 minutos";
+    }
+}
+
+public class BikeRouteStrategy : IRouteStrategy
+{
+    public string BuildRoute(int latitude, int longitude)
+    {
+        return "Bicicleta: 40 minutos";
+    }
+}
+
+public class CarRouteStrategy : IRouteStrategy
+{
+    public string BuildRoute(int latitude, int longitude)
+    {
+        return "Ônibus: 10 minutos";
+    }
+}
