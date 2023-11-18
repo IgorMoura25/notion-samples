f849cbdaa1b28347725d4a08a30c807a73a26ba7
Igor Moura <igor.moura254@hotmail.com>
2023-11-18 16:02:06 -0300

    adding facade pattern

---
+++ architecture-design/design-patterns/GangOfFourPatterns/Structural/Facade/Program.cs
@@ -0,0 +1,11 @@
+﻿using Facade;
+
+Subsystem1 subsystem1 = new Subsystem1();
+Subsystem2 subsystem2 = new Subsystem2();
+var facade = new Facade.Facade(subsystem1, subsystem2);
+
+// O código cliente trabalha com subsistemas complexos através de uma simples
+// interface fornecida pelo Façade. Quando uma fachada gerencia o ciclo de vida
+// do subsistema, o cliente pode nem saber da existência
+// do subsistema. Essa abordagem permite manter a complexidade sob controle.
+Console.Write(facade.Operation());
---
+++ architecture-design/design-patterns/GangOfFourPatterns/Structural/Facade/Structure.cs
@@ -0,0 +1,63 @@
+﻿namespace Facade;
+
+// A classe Façade fornece uma interface simples para a lógica complexa de um
+// ou vários subsistemas. O Façade delega as solicitações do cliente ao
+// objetos apropriados dentro do subsistema. A Fachada também é responsável
+// por gerenciar seu ciclo de vida. Tudo isso protege o cliente da
+// complexidade indesejada do subsistema.
+public class Facade
+{
+    protected Subsystem1 _subsystem1;
+
+    protected Subsystem2 _subsystem2;
+
+    public Facade(Subsystem1 subsystem1, Subsystem2 subsystem2)
+    {
+        _subsystem1 = subsystem1;
+        _subsystem2 = subsystem2;
+    }
+
+    // Os métodos do Façade são atalhos convenientes para as sofisticadas
+    // funcionalidades dos subsistemas. No entanto, os clientes chegam apenas a uma
+    // fração das capacidades de um subsistema.
+    public string Operation()
+    {
+        string result = "Facade initializes subsystems:\n";
+        result += _subsystem1.operation1();
+        result += _subsystem2.operation1();
+        result += "Facade orders subsystems to perform the action:\n";
+        result += _subsystem1.operationN();
+        result += _subsystem2.operationZ();
+        return result;
+    }
+}
+
+// O subsistema pode aceitar solicitações da fachada ou do cliente
+// diretamente (recomendado que seja sempre da Fachada).
+// De qualquer forma, para o Subsistema, a Fachada é mais um cliente, e não faz parte do Subsistema.
+public class Subsystem1
+{
+    public string operation1()
+    {
+        return "Subsystem1: Faz alguma coisa!\n";
+    }
+
+    public string operationN()
+    {
+        return "Subsystem1: Faz alguma outra coisa!\n";
+    }
+}
+
+// Some facades can work with multiple subsystems at the same time.
+public class Subsystem2
+{
+    public string operation1()
+    {
+        return "Subsystem2: Faz tal coisa!\n";
+    }
+
+    public string operationZ()
+    {
+        return "Subsystem2: Faz tal outra coisa!\n";
+    }
+}
