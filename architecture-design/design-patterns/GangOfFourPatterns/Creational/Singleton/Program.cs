09652c6b32d195c2290ac37e325995f03354ac39
Igor Moura <igor.moura254@hotmail.com>
2023-11-20 14:40:14 -0300

    adding singleton pattern

---
+++ architecture-design/design-patterns/GangOfFourPatterns/Creational/Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿// The client code.
+
+Console.WriteLine(
+    "{0}\n{1}\n\n{2}\n",
+    "If you see the same value, then singleton was reused (yay!)",
+    "If you see different values, then 2 singletons were created (booo!!)",
+    "RESULT:"
+);
+
+Thread process1 = new Thread(() =>
+{
+    TestSingleton("FOO");
+});
+Thread process2 = new Thread(() =>
+{
+    TestSingleton("BAR");
+});
+
+process1.Start();
+process2.Start();
+
+process1.Join();
+process2.Join();
+
+
+static void TestSingleton(string value)
+{
+    Singleton.Singleton singleton = Singleton.Singleton.GetInstance(value);
+    Console.WriteLine(singleton.Value);
+}
---
+++ architecture-design/design-patterns/GangOfFourPatterns/Creational/Singleton/Structure.cs
@@ -0,0 +1,47 @@
+﻿namespace Singleton;
+
+// Esta implementação Singleton é chamada de "bloqueio de verificação dupla". É seguro
+// em ambiente multithread e fornece inicialização lenta para o Objeto singleton.
+class Singleton
+{
+    // We'll use this property to prove that our Singleton really works.
+    public string Value { get; set; }
+
+    private static Singleton? _instance;
+
+    // Construtor
+    private Singleton() { }
+
+    // Agora temos um objeto lock que será usado para sincronizar threads
+    // durante o primeiro acesso ao Singleton.
+    private static readonly object _lockInstance = new object();
+
+    public static Singleton GetInstance(string value)
+    {
+        // Esta condicional é necessária para evitar que threads tropecem no
+        // bloqueio quando a instância estiver pronta.
+        if (_instance == null)
+        {
+            // Agora imagine que o programa acaba de ser lançado. Desde
+            // ainda não há instância Singleton, vários threads podem
+            // passe simultaneamente pela condicional anterior e alcance esta
+            // ponto quase ao mesmo tempo. O primeiro deles adquirirá
+            // bloqueie e prosseguirá, enquanto o resto esperará aqui.
+            lock (_lockInstance)
+            {
+                // A primeira thread a adquirir o bloqueio chega a este
+                // condicional, entra e cria o Singleton instância.
+                // Depois de sair do bloco de bloqueio, um thread que
+                // poderia estar esperando pela liberação do bloqueio pode então
+                // entrar nesta seção. Mas como o campo Singleton já está inicializado,
+                // a thread não criará um novo objeto.
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                    _instance.Value = value;
+                }
+            }
+        }
+        return _instance;
+    }
+}
